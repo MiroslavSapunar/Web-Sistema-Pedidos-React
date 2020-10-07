9256916c42f7a7f2f20dd9a620fc28580b62e3e2
Miroslav <psapunar@fi.uba.ar>
2020-10-07 19:12:46 -0300

    clean

--- webpage-react/src/components/index.js
+++
@@ -1,8 +0,0 @@
-import About from './About';
-import Login from './Login';
-import Monitor from './Monitor';
-import Nav from './Nav';
-import Home from './Home';
-import Workshop from './Workshop';
-
-export { About, Login, Monitor, Home, Nav };
