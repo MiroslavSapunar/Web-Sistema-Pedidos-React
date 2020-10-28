2e6ee5d3c19d6e9108652202a018f903788a376d
Miroslav <psapunar@fi.uba.ar>
2020-10-28 20:39:38 -0300

    BottonBar fix

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import {Row, Col, Navbar, Image} from'react-bootstrap'; 
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Row, Col, Navbar, Image } from 'react-bootstrap';
+import { FaFacebookSquare, FaInstagram, FaDiscord} from 'react-icons/fa';
 
 export default class BottomBar extends Component {
 
     render() {
         return (
-            <Navbar  bg="dark" variant="dark" expand="sm" fixed="bottom">
-               <Row className="justify-content-around align-items-center">
-                    <Col className='col-2'>
-                        <a href="https://www.facebook.com/mli.fiuba" target="blank">
-                            <Image fluid src="/img/logopresidenciacei.png" />
-                        </a>
-                        </Col>
+            <Navbar bg="dark" variant="dark" expand="sm" fixed="bottom">
+                <Row className="justify-content-around align-items-center">
+                    <Col className='col-4'>
+                        <Row className='align-items-center'>
+                            <Col clasName='col-auto'>
+                                <Image fluid src="/img/logopresidenciacei.png" />
+                            </Col>
+
+                            <Col className='col-2'>
+                                <a href="https://www.facebook.com/mli.fiuba" target="white">
+                                    <FaFacebookSquare size={30} style={{ fill: 'white' }} />
+                                </a>
+                            </Col>
+
+                            <Col className='col-2'>
+                                <a href="https://www.instagram.com/mli.fiuba" target="white">
+                                    <FaInstagram size={30} style={{ fill: 'white' }} />
+                                </a>
+                            </Col>
+
+                            <Col className='col-2'>
+                                <a href="https://discord.gg/zJB96cf" target="white">
+                                    <FaDiscord size={30} style={{ fill: 'white' }} />
+                                </a>
+                            </Col>
+                        </Row>
+                    </Col>
 
                     <Col className='col-auto'>
                         <Row>
@@ -22,9 +42,6 @@ export default class BottomBar extends Component {
                             <label className="text-white">de la Facultad de Ingeniería de la Universidad de Buenos Aires</label>
                         </Row>
                     </Col>
-
-
-
                 </Row>
             </Navbar>
         );
